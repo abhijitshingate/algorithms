2d815f62bac6b43dd4de829d99934eb33c41fafb
abhijit.shingate@gmail.com <abhijit.shingate@gmail.com>
2012-08-26 15:09:31 +0530

    Problem Statement: Given a string, print all the combinations of that string. e.g. If the input is "ABC" 	 Then output should be A, B, C, AB, BC, CD, BD, ABC

---
+++ src/main/java/com/saansol/algorithms/recursion/StringCombination.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.saansol.algorithms.recursion;
+
+/**
+ * 
+ * 
+ * <b>Problem Statement:</b> Given a string, print all the combinations of that
+ * string. e.g. if the input is "ABC" Then output should be A, B, C, AB, BC, CD,
+ * BD, ABC
+ * 
+ * @author Abhijit Shingate
+ * 
+ */
+public class StringCombination {
+
+	public static void combination(String input, String current, int lastIndex) {
+		int length = input.length();
+		if (lastIndex >= length - 1) {
+			return;
+		}
+		for (int i = lastIndex + 1; i < length; i++) {
+			String x = current + input.charAt(i);
+			System.out.println(x);
+			combination(input, x, i);
+		}
+	}
+
+}
---
+++ src/test/java/com/saansol/algorithms/TestStringCombination.java
@@ -0,0 +1,13 @@
+package com.saansol.algorithms;
+
+import com.saansol.algorithms.recursion.StringCombination;
+
+import junit.framework.TestCase;
+
+public class TestStringCombination extends TestCase {
+
+	public void testCombination() {
+		StringCombination.combination("ABCD", "", 0);
+	}
+
+}
